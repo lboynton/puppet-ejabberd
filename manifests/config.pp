8a936f7cf731d3d2e20bc1833c0a0db45a5bc294
Lee Boynton <lee@lboynton.com>
2014-01-06 14:15:37 +0000

    Add our own ejabberd config

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,3 +1,9 @@
-class ejabberd::config{
-
+class ejabberd::config {
+    file { '/etc/ejabberd/ejabberd.cfg':
+        ensure  => present,
+        owner   => 'ejabberd',
+        group   => 'ejabberd',
+        mode    => '0640',
+        content => template('ejabberd/ejabberd.cfg.erb'),
+    }
 }
