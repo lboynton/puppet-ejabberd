5cb0ffdd6b02a64e118edc8812df053429f8240f
Lee Boynton <lee@lboynton.com>
2014-01-16 14:59:29 +0000

    Set copyright name

--- manifests/init.pp
+++ manifests/init.pp
@@ -33,7 +33,7 @@
 #
 # === Copyright
 #
-# Copyright 2014 Your name here, unless otherwise noted.
+# Copyright 2014 Lee Boynton, unless otherwise noted.
 #
 class ejabberd(
     $config         = $ejabberd::params::config,
